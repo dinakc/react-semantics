0b042ba23a0e616eabc2449df0918f2ba131cf2c
Deena KC <dinakc14@gmail.com>
2023-05-19 00:21:41 -0400

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <h1>React Semantics</h1>
+      
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad fuga cumque ipsam, dolores perferendis quaerat voluptatum veniam architecto id suscipit soluta vero ipsum fugit aut laborum sit alias tenetur vel?</p>
+      <img src="https://www.itprotoday.com/sites/itprotoday.com/files/styles/article_featured_retina/public/programming%20evolution.jpg?itok=WTj9-yNz" alt="img of coding"></img>
+      <footer> Date 05/19/2022</footer>
     </>
   )
 }
